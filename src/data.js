a6c9c7c602e9cb07b1145ee471c8e12a93b02556
shoebshaikh555 <shoeb.shaikh@5d.solutions>
2022-04-25 23:57:36 +0530

    Added categories on home page

--- src/data.js
+++ src/data.js
@@ -1,6 +1,10 @@
 import model2 from "./assets/images/model2.jpg";
 import model3 from "./assets/images/model3.jpg";
 import model4 from "./assets/images/model4.jpg";
+import formals from "./assets/images/formals.jpg";
+import jackets from "./assets/images/jackets.jpg";
+import casuals from "./assets/images/casuals.jpg";
+
 export const sliderItems = [
   {
     id: 1,
@@ -22,5 +26,23 @@ export const sliderItems = [
     title: "Popular Sale",
     description: "Don't compromise on style! Get flat 30% off for new arrivals",
     bg: "fbf0f4",
+  },
+];
+
+export const categories = [
+  {
+    id: 1,
+    img: formals,
+    title: "Formals",
+  },
+  {
+    id: 2,
+    img: jackets,
+    title: "Jackets",
+  },
+  {
+    id: 3,
+    img: casuals,
+    title: "Casuals",
   },
 ];
